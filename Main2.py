f0e0ed9008b3207d45a07d9d6d0bb38e8028a3a1
SneakyPrawnxD <105672860+SneakyPrawnxD@users.noreply.github.com>
2022-05-17 19:37:25 +0200

    Update Main2.py

--- Main2.py
+++ Main2.py
@@ -6,7 +6,11 @@ load_dotenv(find_dotenv())
 
 WebURL = os.environ.get('site')
 LoopCount = int(os.environ.get('times'))
+Totaltime = 0
 
 for i in range(0,LoopCount):
     x = requests.get(WebURL)
+    callTime = x.elapsed
+    Totaltime = callTime + callTime
     print(x.elapsed,x.url)
+print('Total time:' + str(Totaltime))
