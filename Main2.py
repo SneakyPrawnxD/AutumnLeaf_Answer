5cdb4f0003f815a8785891317b9144478b022adc
SneakyPrawnxD <105672860+SneakyPrawnxD@users.noreply.github.com>
2022-05-18 10:12:47 +0200

    Update Main2.py

--- Main2.py
+++ Main2.py
@@ -1,16 +1,20 @@
 import os
 import requests
+
+from functools import total_ordering
+from datetime import timedelta
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 WebURL = os.environ.get('site')
 LoopCount = int(os.environ.get('times'))
-Totaltime = 0
+Totaltime = timedelta(0,0,0)
 
 for i in range(0,LoopCount):
     x = requests.get(WebURL)
     callTime = x.elapsed
-    Totaltime = callTime + callTime
+    Totaltime = Totaltime + callTime
+    
     print('Request time: ' + str(x.elapsed),'| Web URL: ' + str(x.url))
 print('Total time: ' + str(Totaltime))
